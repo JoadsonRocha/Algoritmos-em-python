217e69d6c36a5d8e4bbdddab8d3cd8ad5264e19b
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2023-03-05 16:30:50 -0400

    Create login.py
    Algo de Login simples

---
+++ login.py
@@ -0,0 +1,9 @@
+login = input('Digite seu Email: ')
+password = input('Digite sua Senha: ')
+
+if login == "" or password == "":
+    print('Login ou Senha Digitados Errados')
+else:
+    print('Parabéns: LOGIN')
+
+
