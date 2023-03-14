741832a3017bb8685a23ca8ffc5dad85a0016a0d
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2023-03-13 21:19:44 -0400

    mais algoritmos

---
+++ sorteio.py
@@ -0,0 +1,6 @@
+import math
+import random
+#lista = ['joadson', 'maria', 'pedro']
+num = int(input('Quantas Pessoas? '))
+numero_sorteado = random.randint(1, num)
+print("O número sorteado foi:", numero_sorteado)
