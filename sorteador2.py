fa6db6a7ea5a9f61f08bc6426f2036c5989b6eb1
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2023-03-16 12:41:54 -0400

    algo

---
+++ sorteador2.py
@@ -0,0 +1,8 @@
+import random
+
+participantes = ['Joadson', 'Levi', 'Miguel', 'Jaqueline', 'Marta', 'Simone', 'Sara', 'Rafaela', 'Igor']
+# Sorteia um participante aleatório
+sorteado = random.choice(participantes)
+
+# Imprime o nome do participante sorteado
+print("O participante sorteado é:", sorteado)
