f020f803a376380b0c61d601d4954f771e6e7daa
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2023-03-27 01:36:14 -0400

    Create PintarParede.py

---
+++ PintarParede.py
@@ -0,0 +1,7 @@
+largura = float(input('Qual a Largura da Parede? '))
+altura = float(input('Qual a Altura da Parede? '))
+
+area = largura * altura
+tinta = area/2
+print('')
+print(f'Sua parede tem {largura}X{altura} e sua area é: {area}m e você precisa de {tinta} Litros de tinta')
